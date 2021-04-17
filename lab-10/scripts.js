fb94aec6f6aa4ebb1e84f70b7d7a41787c6c1961
Vishal Agarwala <agarwala@me.com>
2021-04-16 21:08:38 -0400

    Added comments

--- lab-10/scripts.js
+++ lab-10/scripts.js
@@ -1,21 +1,26 @@
+// Setup Add Square Button
 let buttonAddSquare = document.createElement('button');
 let buttonAddSquareText = document.createTextNode('Add Square');
 buttonAddSquare.appendChild(buttonAddSquareText);
 buttonAddSquare.className = 'buttonAddSquare';
 
+// Setup array to manage all black squares and a way to track a unique index
 let squareArray = [];
 let uniqueSquareIndex = 1;
 
+// When Add Square is clicked...
 buttonAddSquare.addEventListener('click', function(e) {
     squareArray.push(document.createElement('div'));
     squareArray[squareArray.length-1].className = 'blackSquare';
     squareArray[squareArray.length-1].id = uniqueSquareIndex;
     squareArray[squareArray.length-1].appendChild(document.createTextNode(uniqueSquareIndex));
 
+    // When a square gets clicked...
     squareArray[squareArray.length-1].addEventListener('click', function(e) {
         this.style.backgroundColor = randomColor();
     });
 
+    // When a square gets double-clicked...
     squareArray[squareArray.length-1].addEventListener('dblclick', function(e) {
         let squares = document.getElementsByClassName('blackSquare');
         
@@ -45,6 +50,14 @@ buttonAddSquare.addEventListener('click', function(e) {
     uniqueSquareIndex++;
 });
 
+// Random Color Generator
+let colors = ['red','orange','yellow','green','blue','indigo','violet','teal'];
+
+function randomColor() {
+  return colors[Math.floor(Math.random()*colors.length)];
+}
+
+// Alright let's start this party...
 let container = document.createElement('div');
 container.className = 'container';
 
@@ -52,9 +65,3 @@ window.addEventListener('DOMContentLoaded', function () {
     document.body.appendChild(buttonAddSquare);
     document.body.appendChild(container);
 });
-
-let colors = ['red','orange','yellow','green','blue','indigo','violet','teal'];
-
-function randomColor() {
-  return colors[Math.floor(Math.random()*colors.length)];
-}
